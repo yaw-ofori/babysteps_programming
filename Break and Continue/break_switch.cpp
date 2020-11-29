ad1cb8601c422185cccc3fc56208cf6b403e6221
yaw-ofori <yawoforidarkwah@gmail.com>
2020-11-29 23:01:40 +0000

    Add files via upload

---
+++ Break and Continue/break_switch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+int selector = 1;
+
+switch (selector) {
+    case 1:
+        cout << "case 1 " << endl;
+    break;
+    case 2:
+        cout << "case 2 " << endl;
+    break;
+
+    default:
+        cout << "default " << endl;
+}
+    return 0;
+}
+
